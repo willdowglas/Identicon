abf0ac9ea1f5174c2f06439c1a5d2ac95bac12b7
Will Douglas <willdowglas@gmail.com>
2020-03-10 23:06:29 -0300

    finishing Identicon module with functions to draw image and save file

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,8 +1,4 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for `Identicon`.
-  """
-  
   require Integer
 
   def main(input) do
@@ -12,17 +8,34 @@ defmodule Identicon do
     |> build_grid
     |> remove_odd_squares
     |> build_pixel_map
+    |> draw_image
+    |> save_file(input)
+  end
+
+  def save_file(image, filename) do
+    File.write("#{filename}.png", image)
+  end
+
+  def draw_image(%Identicon.Image{ color: color, pixel_map: pixel_map  }) do
+    image = :egd.create 250, 250
+    fill = :egd.color color
+
+    Enum.each pixel_map, fn({ start, stop }) ->
+      :egd.filledRectangle image, start, stop, fill
+    end
+
+    :egd.render image
   end
 
   def build_pixel_map(%Identicon.Image{ grid: grid } = image) do
-    pixel_map = Enum.map grid, fn({_number, index}) ->
+    pixel_map = Enum.map grid, fn({ _number, index }) ->
       horizontal = rem(index, 5) * 50
       vertical = div(index, 5) * 50
 
       top_left = { horizontal, vertical }
       bottom_right = { horizontal + 50, vertical + 50  }
 
-        { top_left, bottom_right }
+      { top_left, bottom_right }
     end
 
     %Identicon.Image{ image | pixel_map: pixel_map  }
@@ -32,7 +45,7 @@ defmodule Identicon do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
 
-    %Identicon.Image{hex: hex}
+    %Identicon.Image{ hex: hex }
   end
 
   def pick_color(%Identicon.Image{ hex: [r, g, b | _tail] } = image) do
